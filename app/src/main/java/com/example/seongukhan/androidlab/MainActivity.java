25b7339e20a65556c6cdfc319070382482039e74
SeongUk Han <hsw1824@gmail.com>
2019-01-22 11:28:40 +0900

    test code 1

--- app/src/main/java/com/example/seongukhan/androidlab/MainActivity.java
+++ app/src/main/java/com/example/seongukhan/androidlab/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.seongukhan.androidlab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btn;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn);
+        editText=findViewById(R.id.edit);
+
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String data=editText.getText().toString();
+        Log.d("editText",data);
     }
 }
